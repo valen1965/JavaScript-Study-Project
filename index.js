2427a2241fa53d2c5a197733458a5870e6a61d81
Vlad Vazhnenko <vladvazhnenko@gmail.com>
2022-08-27 01:55:06 +0300

    5-Objects.

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-let name = 'Mosh'; //String literal
-let age = '30'; //Number literal
-let isApproved = trues; //Boolean literal
-let firstName; // if not initialized that Underfined literal
-let lastname = null; // Null literal ( used when we need explicitely clear value of a variable) 
+// let name = 'Mosh'; //String literal
+// let age = 30; //Number literal
+// let isApproved = true; //Boolean literal
+// let firstName = undefined; // if not initialized that Underfined literal
+// let lastname = null; // Null literal ( used when we need explicitely clear value of a variable) 
 
 // 3 - Primitive types
 
@@ -11,3 +11,34 @@ let lastname = null; // Null literal ( used when we need explicitely clear value
 //Boolean
 //undefined
 //null
+
+// 4 - Dynamic Typing
+
+// 5 - Objects
+
+// 5 Reference Types
+
+// - Objects
+// - Arrays
+// - Functions
+
+//let name = 'Mosh';
+//let age = 30;
+
+// we will declare person object instead of two variable
+
+let person = {
+    name: 'Mosh',
+    age: 30
+};
+
+//Lets assume that we want to chnage name
+
+// Dot Notation
+person.name = 'John';
+
+// Bracket Notation
+let selection = 'name';
+person[selection] = 'Mary';
+
+console.log(person.name);    
