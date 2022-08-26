52f2e3ac8caa5ad061386e949fd9fdd5b9963a48
Vlad Vazhnenko <vladvazhnenko@gmail.com>
2022-08-27 01:10:09 +0300

    1-Variables.

--- index.js
+++ index.js
@@ -1,2 +1,26 @@
 // This is my first JavaScript code!
 console.log('Hellow World');
+
+//1- Variables 
+
+//lately expression var to declare variable is no longer used
+// expression let is therfore used to declare variables.
+let name ='Vlad'; 
+console.log(name);
+
+//A few rules for variable declaration:
+
+//1-cann not be a reserved keyword.
+//2-should be meaningful
+//3-can not start with a number
+//4-can not contain a space of hyphen (-)
+//5-are case sensitive
+//6-if we need to declare a few variables we can do this this
+// in single line, separated by coma
+
+//  let firstName = 'Mosh', lastName = 'Hamedani';
+
+//however, modern best parctice is to declare each variable ona new line:
+
+let firstName = 'Mosh';
+let lastName = 'Hamedani';
