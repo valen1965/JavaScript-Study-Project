8d7419c3296c5f4fc2fbc30e6f639ed87e9e477c
Vlad Vazhnenko <vladvazhnenko@gmail.com>
2022-08-28 00:01:49 +0300

    01 - If .. else statement

--- index.js
+++ index.js
@@ -1,21 +1,18 @@
 
-// Swapping Variables
-let a = 'red';
-let b = 'blue';
+// If ..else  statement
 
-console.log(a);
-console.log(b); 
+// Hour
+// If Hour is between 06am and 12pm: Good Morning! 
+// If Hour is between 12pm and 6pm: Good Afternoon!
+// Otherwise : Good Evening!
 
-let c = a; //red
-console.log(c);
-a = b; //blue
-console.log(a = b);
-b = c; //red
-console.log(b = c);
+let hour = 12;
 
-console.log(a);
-console.log(b);
-
-
-
-
+if (hour >=6 && hour < 12) 
+    console.log('Good Morning!');
+else if (hour >=12 && hour < 18) 
+    console.log('Good Afternoon!');
+else if (hour >=18 && hour < 24)
+    console.log('Good Evening!');
+else 
+    console.log('Good Night!');
