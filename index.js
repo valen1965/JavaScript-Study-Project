0af01375db8914fb26854182114e57089d889199
Vlad Vazhnenko <vladvazhnenko@gmail.com>
2022-08-28 00:33:45 +0300

    02-Switch case.

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
 
-// If ..else  statement
+let role = 'guest';
 
-// Hour
-// If Hour is between 06am and 12pm: Good Morning! 
-// If Hour is between 12pm and 6pm: Good Afternoon!
-// Otherwise : Good Evening!
+switch(role){
+    case 'guest':
+        console.log('Guest User');
+        break;
 
-let hour = 12;
+    case 'moderator':
+        console.log('Moderator User');
+        break;
 
-if (hour >=6 && hour < 12) 
-    console.log('Good Morning!');
-else if (hour >=12 && hour < 18) 
-    console.log('Good Afternoon!');
-else if (hour >=18 && hour < 24)
-    console.log('Good Evening!');
-else 
-    console.log('Good Night!');
+    default:    
+        console.log('Unknown User');
+}
+
+if (role === 'guest') console.log('Guest User');
+else if (role === 'moderator') console.log('Moderator User');
+else console.log('Unknown User');
