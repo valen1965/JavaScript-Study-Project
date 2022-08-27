ebc8215f0bcbd2f9977d93cd35902fc3f55a46ee
Vlad Vazhnenko <vladvazhnenko@gmail.com>
2022-08-28 02:07:25 +0300

    08-For..of loops.

--- index.js
+++ index.js
@@ -11,10 +11,8 @@ const person = {
 
 for (let key in person)
     console.log(key, person[key]);
-
+    
+    // for-of loop
 const colors = ['red', 'green', 'blue'];
-
-for(let index in colors)
-    console.log(index, colors[index]);
-
-// for-of loop
+for(let color of colors)
+    console.log(color);
