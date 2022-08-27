4f91f9255f5c2e2cc87088285134f8c6050e28f1
Vlad Vazhnenko <vladvazhnenko@gmail.com>
2022-08-28 01:45:08 +0300

    05-Do While ..loop

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 
 // for (let i = 0; i <= 5; i++) {
-//      if (i % 2 !== 0) console.log(i);
+//       if (i % 2 !== 0) console.log(i);
 // }
 
-let i = 0;
-while (i <=5) {
+// let i = 9;
+// while (i <=5) {
+//     if (i % 2 !== 0) console.log(i);
+//     i++;
+// }
+
+//do -while
+
+let i = 9;
+do {
     if (i % 2 !== 0) console.log(i);
     i++;
-}
+} while (i <= 5);
