d09520f45ba60cb74a8708526c21cfd2bd088372
Vlad Vazhnenko <vladvazhnenko@gmail.com>
2022-08-27 22:31:39 +0300

    10-Operators Precedence.

--- index.js
+++ index.js
@@ -1,32 +1,8 @@
 
-// Bitwise operators.
+// Operators Precedence 
 
-// numbers in decimal system as follows:
+let x = 2 + 3 * 4;
+console.log(x);
 
-// 1 = 00000001   ( 8 digits = 8 bits = 1 byte  )
-// 2 = 00000010  
-// 3 = 00000011 
-// R = 00000000
-
-// Assumming that we nned to implement accec control system
-// that means, user need access to  Read, Write, Execute.
-// Read, Write, Execute
-// 00000100 for Read permission
-// 00000010 for Write permission
-// 00000001 for Execute permission
-
-
-console.log(1 | 2);  // Bitwise OR
-console.log(1 & 2);  // Bitwise AND
-
-const readPermission = 4;  // 00000100 we can use on-line converter to get decimal number 
-const writePermission = 2;
-const executePermission = 1;
-
-let myPermission = 0;
-myPermission = myPermission | writePermission;
-console.log(myPermission);
-
-let message = 
-(myPermission & readPermission) ? 'yes': 'no'
-console.log(message);
+let y = (2 + 3) * 4;
+console.log(y);
