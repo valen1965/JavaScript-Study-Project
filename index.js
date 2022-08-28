3fbcd32c7942c75bf029faba277c0bd01de16af8
Vlad Vazhnenko <vladvazhnenko@gmail.com>
2022-08-28 13:30:40 +0300

    10-Exercise - Max Two Numbers.

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
+// Option 1
 
-let i = 0;
-while(i<=10) {
-    // if(i===5) break;
-    if(i % 2 === 0) {
-        i++;
-        continue;
-    }
-    console.log(i);
-    i++;
+// let num1 = 35;
+// let num2 = 20;
+
+// if (num1 > num2)
+//     console.log(num1);
+//     else console.log(num2);
+
+// Option 2
+
+// let number = max(234, 234);
+// console.log(number);
+
+// function max (num1, num2) {
+//     if (num1 > num2) return num1;
+//     else return num2; 
+// }   
+
+// function max (num1, num2) {
+//   if (num1 > num2) return num1;
+//   return num2; 
+// }   
+
+// Final clean option
+
+let number = max(41, 24);
+console.log(number);
+
+function max (num1, num2) {
+return (num1 > num2) ? num1: num2;
 }
