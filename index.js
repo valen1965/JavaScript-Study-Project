e0a7a31cf108eafed7f9d8d549d39e01e14a000c
Vlad Vazhnenko <vladvazhnenko@gmail.com>
2022-08-30 00:05:25 +0300

    13- Exercise - Demerit Points.

--- index.js
+++ index.js
@@ -1,25 +1,44 @@
+// Exercise - Speed Limit
 
-// Divisible by 3 => Fizz
-// Divisible by 5 => Buzz
-// Divisible by both 3 & 5 => FizzBuzz
-// Not divisible by 3 or 5 => input
-// Not a number => 'Not a number'
+// Speed Limit = 70
+// 5 -> 1 point
+// math.floor(1.3)
+// 12 pints -> suspended.
 
-function fizzBuzz(input) {
-    if (typeof input !== 'number') {
-        return NaN  //'Not a number';
-    }
-    if ((input % 3 === 0) && (input % 5 === 0)) {
-        return 'FizzBuzz';
-    }
-    if (input % 3 === 0) {
-        return 'Fizz';
-    }
-    if (input % 5 === 0) {
-        return 'Buzz';
-    }
-    return input;
-};
 
-const output = fizzBuzz(15);
-console.log(output);
+const speedLimit = 70;
+const kmPerPoint = 5;
+const output = checkSpeed(120)
+
+console.log(output)
+
+// function checkSpeed(speed) {
+    
+//     if (speed < speedLimit + kmPerPoint) {
+//         return ('Ok');
+//     }
+//     else {
+//         const points = Math.floor ((speed - speedLimit) / kmPerPoint);
+//         if (points >= 12) 
+//             return ('License suspended');
+//         else
+//             return ('Points ' +  points);
+//     }
+// }  
+
+
+// making code cleaner
+
+function checkSpeed(speed) {
+    
+    if (speed < speedLimit + kmPerPoint) {
+        return ('Ok');
+        return;
+    }
+    const points = Math.floor ((speed - speedLimit) / kmPerPoint);
+    if (points >= 12) 
+        return ('License suspended');
+    else
+        return ('Points ' +  points);
+    }
+ 
