e5320bcdcbb1e14bda2577a1a00232a80d1a4476
Vlad Vazhnenko <vladvazhnenko@gmail.com>
2022-08-31 13:36:21 +0300

    18- Exercise - Average Grade calculation.

--- index.js
+++ index.js
@@ -1,18 +1,45 @@
 
-// Exercise  - Sum of multiples of 3 and 5
+// Exercise  - Count Grades
+const marks = [80, 80, 50]; 
 
-//multiples of 3: 3,6,9
-//multiples of 5: 5,10
+console.log(calculatedGrade(marks));
 
-const multiple = sum(10) 
-console.log(multiple);
+// Code writted by me.
 
-function sum(limit){
-  let sum = 0;
+// function calculatedGrade(marks){
+// let count = 0;
+// let sum = 0;  
 
-  for (let i = 0; i <= limit; i++) 
-    if (i % 3 === 0 || i % 5 === 0)
-      sum += i;
+// for (let i in marks) 
+//   count ++; {
+// for (let value of marks)
+//   sum += value;
+
+// let grade = sum / count;
+ 
+//   if (grade >=0 &&  grade <=59) return 'F';
+//   if (grade >=60 &&  grade <=69) return 'D';  
+//   else if (grade >=70 &&  grade <=79) return 'C';  
+//   else if (grade >=80 &&  grade <=89) return 'B';  
+//   else return ('A');  
+    
+//   }
   
-  return sum;
+// }  
+
+function calculatedGrade(marks){
+  const average = calculateAverage(marks);
+
+  if (average < 60) return 'F';
+  if (average < 70) return 'D';
+  if (average < 80) return 'C';
+  if (average < 90) return 'B';
+  return 'A';
+}
+// reusable function for calculating average
+function calculateAverage(array) {
+  let sum =0;
+  for (let value of array)
+    sum += value;
+  return sum / array.length;
 }
