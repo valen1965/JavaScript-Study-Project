bd3d029269e9b449dec94a79e4118eadc4be1135
Vlad Vazhnenko <vladvazhnenko@gmail.com>
2022-08-31 14:19:51 +0300

    19-Exercis e- 10 Stars.

--- index.js
+++ index.js
@@ -1,45 +1,15 @@
 
-// Exercise  - Count Grades
-const marks = [80, 80, 50]; 
+// Exercise  - Show Stars
 
-console.log(calculatedGrade(marks));
+showStars(10)
 
-// Code writted by me.
-
-// function calculatedGrade(marks){
-// let count = 0;
-// let sum = 0;  
-
-// for (let i in marks) 
-//   count ++; {
-// for (let value of marks)
-//   sum += value;
-
-// let grade = sum / count;
- 
-//   if (grade >=0 &&  grade <=59) return 'F';
-//   if (grade >=60 &&  grade <=69) return 'D';  
-//   else if (grade >=70 &&  grade <=79) return 'C';  
-//   else if (grade >=80 &&  grade <=89) return 'B';  
-//   else return ('A');  
-    
-//   }
-  
-// }  
-
-function calculatedGrade(marks){
-  const average = calculateAverage(marks);
-
-  if (average < 60) return 'F';
-  if (average < 70) return 'D';
-  if (average < 80) return 'C';
-  if (average < 90) return 'B';
-  return 'A';
+function showStars(rows) {
+  for (let row = 1; row <= rows; row++){
+    let pattern = '';
+    for (let i = 0; i < row; i++)
+     pattern += '*';
+  console.log(pattern);   
+  }
 }
-// reusable function for calculating average
-function calculateAverage(array) {
-  let sum =0;
-  for (let value of array)
-    sum += value;
-  return sum / array.length;
-}
+
+
