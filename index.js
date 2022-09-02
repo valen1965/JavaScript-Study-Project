62d74a23fe573627555348fa48c424b5739557e5
Vlad Vazhnenko <vladvazhnenko@gmail.com>
2022-09-03 02:00:01 +0300

    Exercise 4 - Blog Post Object.

--- index.js
+++ index.js
@@ -1,28 +1,16 @@
 
-// 15 - Excercise 3 - Object Equality.
+// 18 - Excercise 4 - Blog Post Object.
 
+let post = {
+  title: 'alfa',
+  body: 'body',
+  author: 'human',
+  views: 10,
+  comments: [
+    { author: 'A', body: 'B'},
+    { author: 'B', body: 'C'},
+  ],
+  isLive: true
+};
 
-//Constructor Function
-const address1 = new Address('a', 'b', 'c');
-const address2 = new Address('a', 'b', 'c');
-const address3 = address1;
-
-console.log(areEqual(address1, address2));
-console.log(areSame(address1, address2));
-console.log(areSame(address1, address3));
-
-function Address (street, city, zipCode){
-  this.street = street;
-  this.city = city;
-  this.zipCode = zipCode;
-}
-
-function areEqual(address1, address2) {
-  return address1.street === address2.street && 
-  address1.city === address2.city && address1.zipCode === address2.zipCode;
-}
-
-function areSame(address1, address2) {
-  return address1 === address2;
-
-}
+console.log(post);
