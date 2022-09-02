eb49295566723c242003e63b8d72c8347cbd6539
Vlad Vazhnenko <vladvazhnenko@gmail.com>
2022-09-02 19:29:58 +0300

    OBJECTS 6-Functions are objects.

--- index.js
+++ index.js
@@ -1,26 +1,23 @@
 
 // Chapter OBJECTS
-// 5 - Constructor Property;
+// 6 - Functions are Objects;
 
-// Factory Function
-
-function createCircle(radius) {
-  return {
-    radius,
-    draw() {
-      console.log('draw');
-    }
-  };
-}
-
-const circle = createCircle(1);
-
-// Constructor Function
 function Circle(radius) {
   this.radius = radius;
   this.draw = function() {
     console.log('draw');
   }
 }
+
+// const Circle1 = new Function('radius', ` 
+//   this.radius = radius;
+//   this.draw = function() {
+//     console.log('draw');
+// }  
+// `);
+// const circle = new Circle1(1);
+
+Circle.call({}, 1)  // it is ssame expression like in line 22
+Circle.apply({}, [1])
 
 const another = new Circle(1);
