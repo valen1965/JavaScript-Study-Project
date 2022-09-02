4c377962fd9762b8458ce13482df346bfe45e2cb
Vlad Vazhnenko <vladvazhnenko@gmail.com>
2022-09-03 00:01:17 +0300

    OBJECTS 14- Date.

--- index.js
+++ index.js
@@ -1,20 +1,9 @@
 
 // Chapter OBJECTS
-// 13 - Template Literals.
+// 14 - Date object.
 
-// string primitive
+const now = new Date();
+const date1 = new Date('May 11 2018 09:00');
+const date2 = new Date(2018, 4, 11, 9);
 
-// Object literals {}
-// Boolean literals true or false
-// String literals '', ""
-// Template literals ``
-
-const name = 'John';
-const message = 'Hi ' + name + ',\n';
-
-const another =
-`Hi ${name} ${2 + 3}, 
-Thanks you for joining my mailing list.
-
-Regards,
-Mosh`;
+now.setFullYear(2017);
