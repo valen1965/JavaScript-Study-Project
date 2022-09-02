7bfd4d310c7e91ea35f30955690e04912a08d298
Vlad Vazhnenko <vladvazhnenko@gmail.com>
2022-09-02 22:02:52 +0300

    OBJECTS 11-Math.

--- index.js
+++ index.js
@@ -1,21 +1,4 @@
 
 // Chapter OBJECTS
-// 9 - Cloning an object.
-
-const circle = {
-  radius: 1,
-  draw() {
-    console.log('draw');
-  }
-};
-
-// const another = {};
-//   for (let key in circle) 
-//     another[key] = circle[key];
-  
-//const another = Object.assign({}, circle); // copies properties or methods from one or more source objects, or combine a few objects into a target object.  
-
-const another = {...circle}; // spread operator is used to clone all properties and methods and put them into another object. 
-
-console.log(another);  
+// 11 - Math.
 
