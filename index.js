c5e7578252952ad552e56bfe9f507e07f632405a
Vlad Vazhnenko <vladvazhnenko@gmail.com>
2022-09-03 01:44:03 +0300

    Exercise 3 - Objects Equality.

--- index.js
+++ index.js
@@ -1,22 +1,28 @@
 
-// 15 - Excercise 2 - Factory and Constructor Function.
+// 15 - Excercise 3 - Object Equality.
 
-// let address = createAddress('sivash', 'kherson', 73000);
-// console.log(address);
-// // Factory Function
-// function createAddress(street, city, zipCode) {
-//   return {
-//     street,
-//     city,
-//     zipCode,
-//   };
-// }
 
 //Constructor Function
+const address1 = new Address('a', 'b', 'c');
+const address2 = new Address('a', 'b', 'c');
+const address3 = address1;
+
+console.log(areEqual(address1, address2));
+console.log(areSame(address1, address2));
+console.log(areSame(address1, address3));
+
 function Address (street, city, zipCode){
   this.street = street;
   this.city = city;
   this.zipCode = zipCode;
 }
-const address = new Address('a', 'b', 'c');
-console.log(address);
+
+function areEqual(address1, address2) {
+  return address1.street === address2.street && 
+  address1.city === address2.city && address1.zipCode === address2.zipCode;
+}
+
+function areSame(address1, address2) {
+  return address1 === address2;
+
+}
