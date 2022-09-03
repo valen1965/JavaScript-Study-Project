39d2d073a1542d1817a7a321fb35b1273f326358
Vlad Vazhnenko <vladvazhnenko@gmail.com>
2022-09-03 11:59:06 +0300

    Excercise 5 - Constructor Function.

--- index.js
+++ index.js
@@ -1,16 +1,37 @@
 
 // 18 - Excercise 4 - Blog Post Object.
 
-let post = {
-  title: 'alfa',
-  body: 'body',
-  author: 'human',
-  views: 10,
-  comments: [
-    { author: 'A', body: 'B'},
-    { author: 'B', body: 'C'},
-  ],
-  isLive: true
-};
+// let post = {
+//   title: 'alfa',
+//   body: 'body',
+//   author: 'human',
+//   views: 10,
+//   comments: [
+//     { author: 'A', body: 'B'},
+//     { author: 'B', body: 'C'},
+//   ],
+//   isLive: true
+// };
 
+function Post(title, body, author)  {
+  this.title = title;
+  this.body = body;
+  this.author = author;
+  this.views = 0;
+  this.comments = [];
+  this.isLive = false;
+}
+
+let post = new Post ('TITLE', 'BODY', 'AUTHOR');
 console.log(post);
+
+// let post = {
+//   title: 'alfa',
+//   body: 'body',
+//   author: 'human',
+//   views: 0,
+//   comments: [],
+//   isLive: false
+// };
+
+// console.log(post);
