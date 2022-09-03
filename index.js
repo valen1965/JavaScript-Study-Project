fed7e38623e782c98dab49cbfae090a79d59ac1d
Vlad Vazhnenko <vladvazhnenko@gmail.com>
2022-09-03 14:46:59 +0300

    ARRAYS 05- Arrow Functions.

--- index.js
+++ index.js
@@ -1,17 +1,27 @@
 // ARRAYS
-// 04 - Finding Elements (Reference)
+// 05 - Arrow Functions
+
+// In ES6 there is a shorter and cleaner way to write the same code using Arrow Functions. Wherner you want to pass a function as a callback function or as an argument for a differenet method, use the arrow function syntax.
+
 
 const courses = [
   { id: 1, name: 'a' },
   { id: 2, name: 'b' },
 ];
 
-// const course = courses.find(function(course) {
-//   return course.name === 'b';
-// });
-
-const course = courses.findIndex(function(course) {
-  return course.name === 'a';
-});
+const course = courses.findIndex(course => course.name === 'b');
 
 console.log(course);
+
+const cours = courses.find(cours => cours.name === 'a');
+
+console.log(cours);
+
+
+// Finding Elelents (reference type) using function sytax;
+
+const cour = courses.find(function(cour) {
+  return cour.name === 'a';  
+});
+
+console.log(cour);
