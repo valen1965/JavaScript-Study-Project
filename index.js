d72fbe8ea2fb7a06e527b2e272a8a3ccb2a90ca6
Vlad Vazhnenko <vladvazhnenko@gmail.com>
2022-09-04 00:58:39 +0300

    ARRAYS 14 - Filtering an Array.

--- index.js
+++ index.js
@@ -1,22 +1,8 @@
 // ARRAYS
-// 13 - Testing the Elements of an Array.
+// 14 - Filtering an Array.
 
-const numbers = [1, -2, 3];
+const numbers = [1, -1, 2, 3];
 
-//checking if all numbers in the array are positive
+const filtered = numbers.filter(value => value >= 0);
 
-const allPositive = numbers.every(function(value) {
-    return value >= 0;
-});
-
-console.log(allPositive);
-
-// arrow function
-const allPos = numbers.every(value => value >= 0);
-console.log(allPos);
-
-const atLeastOnePositive = numbers.some(function(value) {
-    return value >= 0;
-});
-
-console.log(atLeastOnePositive);
+console.log(filtered);
