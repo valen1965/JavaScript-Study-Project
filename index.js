fcbe2a30f0ca5a538995e2ee02917c8fb4e3a208
Vlad Vazhnenko <vladvazhnenko@gmail.com>
2022-09-04 13:10:02 +0300

    ARRAYS Excercise 1 - Array from Range.

--- index.js
+++ index.js
@@ -1,28 +1,11 @@
-// ARRAYS
-// 16 - Reducing an Array.
+// 17 - Excercise 1 - Array from Range.
+const numbers = arrayFromRange(1, 10);
 
-const numbers = [1, 15, 22, 3];
+console.log(numbers);
 
-// let sum = 0;
-// for (let value of numbers)
-//   sum += value;
-
-
-// Reduce method
-
-// a = 0, c = 1 => a = 1
-// a = 1, c =15 => a = 16
-// a = 16, c = 22 => a = 38
-// a = 38, c = 3 => a = 41
-
-// const sum = numbers.reduce((accummulator, currentValue) =>{
-//   return accummulator + currentValue;
-// }, 0);
-
-
-// we can make this code shorter.  
-
-const sum = numbers.reduce(
-  (accummulator, currentValue) => accummulator + currentValue);
-
-console.log(sum);  
+function arrayFromRange(min, max) {
+    const output = [];   // creating an empty array
+    for (let i = min; i <= max; i++)
+        output.push(i);
+    return output;
+}
