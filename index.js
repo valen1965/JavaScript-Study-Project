d43a817877432244e30ff262cdb7c1a3660fab11
Vlad Vazhnenko <vladvazhnenko@gmail.com>
2022-09-04 14:24:36 +0300

    ARRAYS Excercise 3 - Except.

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
-// 18 - Excercise 2 - Includes.
-const numbers = [1, 2, 3, 4];
+// 19 - Excercise 3 - Except.
+const numbers = [1, 2, 3, 4, 5, 6];
 
-console.log(includes(numbers, -1));
+const output = except(numbers, [6]);
 
-function includes(array, searchElement) {
-        for (let element of array)
-            if (element === searchElement)
-                return true;
-        return false;         
-}  
+console.log(output);
 
+function except(array, excluded) {
+    const output = [];
+    for (let element of array)
+        if (!excluded.includes(element))
+            output.push(element);
+    return output;
+}
