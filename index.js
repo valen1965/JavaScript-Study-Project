c2f555f3aa615b5061fbb874862d37688b842444
Vlad Vazhnenko <vladvazhnenko@gmail.com>
2022-09-04 16:49:50 +0300

    ARRAYS Excercise 4 - Moving an element amended.

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
-// 19 - Excercise 3 - Except.
+// 20 - Excercise 4 - Moving an element.
 const numbers = [1, 2, 3, 4, 5, 6];
 
-const output = except(numbers, [6]);
+const output = move(numbers, 0, 0);
 
 console.log(output);
 
-function except(array, excluded) {
-    const output = [];
-    for (let element of array)
-        if (!excluded.includes(element))
-            output.push(element);
-    return output;
+function move(array, index, offset) {
+  const position = index + offset;
+   if (position >= array.length || position < 0) {
+    console.error('Invalid offset');
+    return;
+   } 
+
+  const output = [...array];          // copying the array
+  const element = output.splice(index, 1)[0];
+  output.splice(position, 0, element);
+  return output;
 }
