95c86dceb2d7602f5f2b99f1cbeef846cc8eb1bd
Vlad Vazhnenko <vladvazhnenko@gmail.com>
2022-09-08 13:03:57 +0300

    FUNCTIONS 10 - This Keyword.

--- index.js
+++ index.js
@@ -1,29 +1,34 @@
 
-// Functions  09 - Let vs Var.
+// Functions  10 - This Keyword.
 
-// First problem with var declaration:
+// THIS referencing the OBJECT that is executing the current function. 
 
-// function start() {
-//   for (let i = 0; i < 5; i++) {
-//     if(true) {
-//       var color = 'green';
-//     }
-//     console.log(color);
-//   }
-//   console.log(i);
+// if a function is part of an object - we call it method -> obj.
+// if a function is not part of an object - it is global functions / function -> global ( which is window in browser and global in node.js)
+
+const video = {
+  title: 'a',
+  tags: ['c', 'd', 'e'],
+  showTags() {
+    this.tags.forEach(function(tag) {
+      console.log(this.title , tag);
+    }, this ); 
+  }
+};
+
+video.showTags();
+
+
+// video.stop = function() {
+//   console.log(this);
+// };   
+
+// function Video(title) {
+//   this.title = title;
+//   console.log(this);
 // }
 
-// var => function-scoped variable
-// let, const => block-scoped variable
+// const v = new Video('b');
+// // video.stop();
 
-// start();
-
-// Second problem with var declaration:
-
-var color = 'red';
-let age = 30;
-
-function sayHi() {
-  console.log('hi');
-}
-
+// //playVideo();
