5f9f5c4ad5ebe89d22bf761ac68e4051eed7c8e1
Vlad Vazhnenko <vladvazhnenko@gmail.com>
2022-09-08 12:12:33 +0300

    FUNCTIONS 08 - Local vs Global Scope.

--- index.js
+++ index.js
@@ -1,28 +1,15 @@
 
-// Functions  07 - Try & Catch Error Handling.
-const person = {
-  firstName: 'Mosh',
-  lastName: 'Hamedani',
-  get fullName() {
-    return `${person.firstName} ${person.lastName}`
-  },
-  set fullName(value) {
-    if(typeof value !== 'string')
-      throw new Error('Value is not a string');
+// Functions  08 - Local vs Global Scope.
 
-    const parts =value.split(' ');
-      if (parts.length !== 2)
-      throw new Error('Enter first and last name');
-    this.firstName = parts[0];
-    this.lastName = parts[1];
-  }
-};
-
-try {
-  person.fullName = '';
+const color = 'red';
+function start() {
+  const message = 'hi';
+  const color = 'blue';
+  console.log(color);
 }
-catch(e) {
-  alert(e);
-}
-console.log(person);
 
+function stop() {
+  const message = 'bye';
+}
+
+start();
