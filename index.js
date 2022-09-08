fbac7433ee721f296aa20556d63f83bacb840e42
Vlad Vazhnenko <vladvazhnenko@gmail.com>
2022-09-09 02:28:24 +0300

    FUNCTIONS - Excercise 3 - Error handling.

--- index.js
+++ index.js
@@ -1,17 +1,24 @@
 
-//FUNCTIONS - Excercise 2 - Area of Circle.
+//FUNCTIONS - Excercise 3 - Error handling.
 
-const circle = {
-   
-  set newRadius(value) {
-    this.radius = value;
-  },
-  get area() {
-   return (Math.PI * this.radius * this.radius).toFixed(3);
 
+try {
+  const numbers = [1, 2, 3, 4, 4];
+  const count = countOccurrences(null, 1);
+  console.log(count);
+}
+catch(e) {
+ console.log(e.message); 
  }
-};
 
+function countOccurrences(array, searchElement) {
+    if (!Array.isArray(array))
+    throw new Error('Value is not an array.');
 
-circle.newRadius = 5.4;
-console.log(circle.area);
+  return array.reduce((acc, current) => {
+    const occ = (current === searchElement) ? 1: 0;
+  
+    return acc + occ; 
+  },0);
+}
+
