053bf50a0cb651ce77c4eec4f09fcccd0663610a
Vlad Vazhnenko <vladvazhnenko@gmail.com>
2022-09-08 13:45:58 +0300

    FUNCTIONS 11 - Changing This.

--- index.js
+++ index.js
@@ -1,34 +1,54 @@
 
-// Functions  10 - This Keyword.
+// Functions  11 - Changing This.
 
-// THIS referencing the OBJECT that is executing the current function. 
+// const video = {
+//   title: 'a',
+//   tags: ['c', 'd', 'e'],
+//   showTags() {
+//     this.tags.forEach(function(tag) {
+//       console.log(this.title , tag);
+//     }.bind(this));                  // bind method
+//   }
+// };
+// video.showTags();
 
-// if a function is part of an object - we call it method -> obj.
-// if a function is not part of an object - it is global functions / function -> global ( which is window in browser and global in node.js)
 
 const video = {
   title: 'a',
   tags: ['c', 'd', 'e'],
   showTags() {
-    this.tags.forEach(function(tag) {
+    this.tags.forEach(tag => {      // arrow function method inherits this from the containing function
       console.log(this.title , tag);
-    }, this ); 
+    });                   
   }
 };
-
 video.showTags();
 
 
-// video.stop = function() {
-//   console.log(this);
-// };   
 
-// function Video(title) {
-//   this.title = title;
+
+
+
+
+
+
+
+
+
+
+
+
+
+// another approach to change this.
+
+// function playVideo() {
 //   console.log(this);
 // }
 
-// const v = new Video('b');
-// // video.stop();
+// playVideo.call({ name: 'Mosh' }, 1, 2);  // diff withapply method
+// playVideo.apply({ name: 'Mosh' }, [1, 2]); // diff with call method
+// playVideo.bind({ name: 'Mosh' }) ();  // returns a new function and sets THIS to always point to this object permanently.
 
-// //playVideo();
+
+
+// playVideo();
